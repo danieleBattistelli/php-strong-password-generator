9832a1d1bf587be4bbc78377bb85c89588d4e555
danieleBattistelli <bomber81@gmail.com>
2025-03-13 17:11:35 +0100

    feat: Milestone 2

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+function generatePassword($length)
+{
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+    $charactersLength = strlen($characters);
+    $randomPassword = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomPassword .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomPassword;
+}
+?>
--- index.php
+++ index.php
@@ -17,16 +17,7 @@
     </form>
 
     <?php
-    function generatePassword($length)
-    {
-        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
-        $charactersLength = strlen($characters);
-        $randomPassword = '';
-        for ($i = 0; $i < $length; $i++) {
-            $randomPassword .= $characters[rand(0, $charactersLength - 1)];
-        }
-        return $randomPassword;
-    }
+    include 'functions.php';
 
     if (isset($_GET['length'])) {
         $length = intval($_GET['length']);
